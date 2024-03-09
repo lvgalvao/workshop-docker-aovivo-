8abd1da79459c0d078d2de68f4d894e1aa49cbeb
lvgalvaofilho <lvgalvaofilho@gmail.com>
2024-03-09 09:35:23 -0300

    meu app funcionando

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def hello_world():
+    return "Olá Turma de Dados! Aula de Docker"
+
+def main():
+    st.write(hello_world())
+
+if __name__ == "__main__":
+    main()
+ 
